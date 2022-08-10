6702db056d9040361ad2e03d12c00da316a02f91
ShandyHan <108128753+ShandyHan@users.noreply.github.com>
2022-08-10 19:47:49 +0900

    Add files via upload

---
+++ GlobalParticleOnOff.cs
@@ -0,0 +1,37 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.SDK3.Components;
+using VRC.Udon.Common.Interfaces;
+
+public class GlobalParticleOnOff : UdonSharpBehaviour
+{
+    public ParticleSystem particle;
+    public bool isTriggered = false;
+
+    public override void Interact()
+    {
+        if(isTriggered == true)
+        {
+            isTriggered = false;
+            SendCustomNetworkEvent(NetworkEventTarget.All, nameof(GlobalParticleOn));
+        }
+        else
+        {
+            isTriggered = true;
+            SendCustomNetworkEvent(NetworkEventTarget.All, nameof(GlobalParticleOff));
+        }
+    }
+
+    public void GlobalParticleOn()
+    {
+        particle.Play();
+    }
+
+    public void GlobalParticleOff()
+    {
+        particle.Stop();
+    }
+}
